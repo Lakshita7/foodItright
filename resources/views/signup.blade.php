982e63cdfe779edab730d9274caf2f13fda0af04
Lakshita7 <lakshy7malhotra@gmail.com>
2019-06-16 15:01:26 +0530

    Updated signup.blade.php

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -103,7 +103,7 @@
       <label for="psw"><b>Password</b></label>
       <input type="password" placeholder="Enter Password" name="psw" required>
         
-      <button type="submit">Login</button>
+      <button><a href="dashboard" class="signupbtn" type="submit">LOG IN</a></button>
       <label>
         <input type="checkbox" checked="checked" name="remember"> Remember me
       </label>
